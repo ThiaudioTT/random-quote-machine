984368e9acd9e67be14f3dbef7ae674b4f416753
ThiaudioTT <thiago.sky25@gmail.com>
2024-03-12 18:41:15 -0300

    Add quote interface and implement random quote functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
+'use client';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faQuoteLeft, faShuffle } from "@fortawesome/free-solid-svg-icons";
 import { faQuoteRight } from "@fortawesome/free-solid-svg-icons";
 import { faXTwitter } from "@fortawesome/free-brands-svg-icons";
+import { useState } from "react";
+import { IQuote } from "./models/quote.interface";
 
 export default function Home() {
+  const [quote, setQuote] = useState<IQuote>({ content: "I am a quote", author: "Author" });
+
+  function getRandomQuote() {
+    setQuote({ content: "I am a new quote", author: "New Author" })
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center items-center p-24">
       <div
@@ -13,12 +22,14 @@ export default function Home() {
         <div className="quote-flexbox">
           <p id="text" className="font-serif text-5xl italic text-center">
             <FontAwesomeIcon icon={faQuoteLeft} className="w-8" />
-            I am the quote text
+
+            {quote.content}
+
             <FontAwesomeIcon icon={faQuoteRight} className="w-8 ml-auto" />
           </p>
 
           <p id="author" className="ml-auto text-right">
-            ~ Author
+            ~ {quote.author}
           </p>
         </div>
 
@@ -33,7 +44,7 @@ export default function Home() {
           </a>
 
           <button id="new-quote" className="btn flex-item">
-            <FontAwesomeIcon icon={faShuffle} className="w-6"/>
+            <FontAwesomeIcon icon={faShuffle} className="w-6" onClick={getRandomQuote}/>
           </button>
         </div>
       </div>
