1be140876c5ae47f8971c7405e779a22c163a18c
ThiaudioTT <thiago.sky25@gmail.com>
2024-03-13 18:08:02 -0300

    Refactor page.tsx to improve code readability and add new features

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,37 @@
-'use client';
+"use client";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faQuoteLeft, faShuffle } from "@fortawesome/free-solid-svg-icons";
 import { faQuoteRight } from "@fortawesome/free-solid-svg-icons";
 import { faXTwitter } from "@fortawesome/free-brands-svg-icons";
 import { useState } from "react";
 import { IQuote } from "./models/quote.interface";
+import { width } from "@fortawesome/free-brands-svg-icons/fa42Group";
 
 export default function Home() {
-  const [quote, setQuote] = useState<IQuote>({ content: "I am a quote", author: "Author" });
+  const [quote, setQuote] = useState<IQuote>({
+    content: "I am a quote",
+    author: "Author",
+  });
 
   function getRandomQuote() {
     // setQuote({ content: "I am a new quote", author: "New Author" })
 
-    fetch("https://api.quotable.io/random").then(response => {
-      if (!response.ok) throw new Error("Network response was not ok");
-      return response.json();
-    })
-    .then(data => {
-      const quote: IQuote = {
-        content: data.content,
-        author: data.author
-      }
-      setQuote(quote);
-    })
-    .catch(error => {
-      console.error("There was a problem fetching the quote", error);
-      window.alert("There was a problem fetching the quote");
-    })
+    fetch("https://api.quotable.io/random")
+      .then((response) => {
+        if (!response.ok) throw new Error("Network response was not ok");
+        return response.json();
+      })
+      .then((data) => {
+        const quote: IQuote = {
+          content: data.content,
+          author: data.author,
+        };
+        setQuote(quote);
+      })
+      .catch((error) => {
+        console.error("There was a problem fetching the quote", error);
+        window.alert("There was a problem fetching the quote");
+      });
   }
 
   return (
@@ -34,16 +39,20 @@ export default function Home() {
       <div
         id="quote-box"
         className="bg-white p-8 rounded-lg shadow-lg text-black"
+        style={{maxWidth: "900px"}}
       >
         <div className="quote-flexbox">
-          <p id="text" className="font-serif text-5xl italic text-center">
-            <FontAwesomeIcon icon={faQuoteLeft} className="w-8" />
-
-            {quote.content}
-
+          <div className="left-quote text-5xl">
+            <FontAwesomeIcon icon={faQuoteLeft} className="w-8 text-right" />
+          </div>
+          <div className="quote-content">
+            <p id="text" className="font-serif text-5xl italic">
+              {quote.content}
+            </p>
+          </div>
+          <div className="right-quote text-right text-5xl">
             <FontAwesomeIcon icon={faQuoteRight} className="w-8 ml-auto" />
-          </p>
-
+          </div>
           <p id="author" className="ml-auto text-right">
             ~ {quote.author}
           </p>
@@ -52,15 +61,19 @@ export default function Home() {
         <div className="actions-footer mt-8 flex justify-between">
           <a
             id="tweet-quote"
-            className="btn flex-item"
+            className="btn flex-item ml-3"
             href="https://twitter.com/intent/tweet"
             target="_blank"
           >
-            <FontAwesomeIcon icon={faXTwitter} className="w-6"/>
+            <FontAwesomeIcon icon={faXTwitter} size="2x" />
           </a>
 
-          <button id="new-quote" className="btn flex-item">
-            <FontAwesomeIcon icon={faShuffle} className="w-6" onClick={getRandomQuote}/>
+          <button
+            id="new-quote"
+            className="btn flex-item mr-3"
+            onClick={getRandomQuote}
+          >
+            <FontAwesomeIcon icon={faShuffle} size="2x" />
           </button>
         </div>
       </div>
